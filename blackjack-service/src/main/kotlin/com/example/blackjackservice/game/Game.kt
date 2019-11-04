3f11057ac288f367ff12590b09c12d407cbd5b66
Ricardo Gomes <ipsiart@gmail.com>
2019-11-04 17:36:00 +0000

    Black jack developments

---
+++ blackjack-service/src/main/kotlin/com/example/blackjackservice/game/Game.kt
@@ -0,0 +1,10 @@
+package com.example.blackjackservice.game
+
+import com.rfgomes.poker.card.Card
+import java.util.*
+
+class Game(val id: String = UUID.randomUUID().toString(),
+           val cards : Pair<Card, Card>,
+           val ) {
+
+}
