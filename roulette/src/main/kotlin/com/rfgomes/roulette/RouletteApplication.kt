2553f42ff9447e51359805c50070fe22e221ab68
Ricardo Gomes <ipsiart@gmail.com>
2019-11-05 12:13:12 +0000

    init roulette

---
+++ roulette/src/main/kotlin/com/rfgomes/roulette/RouletteApplication.kt
@@ -0,0 +1,11 @@
+package com.rfgomes.roulette
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RouletteApplication
+
+fun main(args: Array<String>) {
+    runApplication<RouletteApplication>(*args)
+}
---
+++ roulette/src/main/kotlin/com/rfgomes/roulette/infrastructure/CassandraConfig.kt
@@ -0,0 +1,2 @@
+package com.rfgomes.roulette.infrastructure
+
